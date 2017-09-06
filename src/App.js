ebd2c0f46f855ba1fae6b8d38ff3a93049a78209
Mitche Guevara <mitche.guevara@gmail.com>
2017-09-06 12:16:47 -0400

    functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
-import Stopwatch from "./Stopwatch";
-import './App.css';
+import Stopwatch from './Stopwatch';
+import './App.css'
+
+
 
 class App extends Component {
   render() {
-    return (
-      <Stopwatch />
-    );
-  }
+  return(
+    <Stopwatch />
+  );
 }
 
+}
 export default App;
+
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       secondsElapsed: 0
+//     };
+//     this.incrementer = null;
+//   }
+//
+//
+//   handleStartClick(e){
+//     setInterval(function() {this.setState({
+//       secondsElapsed: this.state.secondsElapsed + 1
+//     })
+//   }, 1000)
+// }
+//
+//   handleStopClick(e){
+//     clearInterval(this.incrementer);
+//     this.setState({
+//       lastClearedIncrementer: this.incrementer
+//     });
+//   }
+//
+//   handleResetClick(){
+//     clearInterval(this.incrementer);
+//     this.setState({
+//       secondsElapsed:0,
+//       laps:[]
+//     });
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>{this.state.secondsElapsed}</h1>
+//         <button type="button" onClick={e => this.handleStartClick()}>Start</button>
+//         <button type="button" onClick={e => this.handleStopClick(e)}>Stop</button>
+//         <button type="button" onClick={e => this.handleResetClick(e)}>Reset</button>
+//       </div>
+//     );
+//   }
+// }
+//
--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,18 +2,43 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor(){
+    super()
+    this.state = {
+      seconds: 0
+    }
+    this.incrementer = null;
+  }
+
+
+  handleStartClick(e){
+    e.preventDefault()
+    this.incrementer = setInterval(() => {this.setState({
+      seconds: this.state.seconds += 1
+    })
+  }, 1000)
+}
+  handleStopClick(e){
+    e.preventDefault()
+    clearInterval(this.incrementer)
+  }
+  handleResetClick(e){
+    e.preventDefault()
+    clearInterval(this.incrementer)
+    this.setState({seconds:0})
+  }
+
   render() {
     return (
-      <div className="stopwatch">
-        <h1>0</h1>
-        <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
-        </div>
+      <div className="App">
+        <h1>{this.state.seconds}</h1>
+        <button type="button" onClick={e => this.handleStartClick(e)}>Start</button>
+        <button type="button" onClick={e => this.handleStopClick(e)}>Stop</button>
+        <button type="button" onClick={e => this.handleResetClick(e)}>Reset</button>
       </div>
     );
   }
 }
+
 
 export default Stopwatch;
